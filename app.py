2ed4e98f376a8f55a5265e12ebbf0023e8df2df2
Roxanna Rodriguez <roxanna.rdgz@gmail.com>
2024-04-09 18:12:49 +0300

    Update app.py
    Updated charts layouts, labels and legends. Added bottom container with selection. Updated texts and titles.

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Import necessary data
+# Import necessary data - (data after changing data types, adding new columns and filling null values)
 vehicles_data = pd.read_csv('notebook/vehicles_clean.csv')
 
 st.set_page_config(page_title='Vehicle Explorer', page_icon=':car:', layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 # Header -----------------------------------------------------
-st.title("Vehicle Explorer")
+st.title("Vehicle Data Explorer")
 
 
-with st.expander(('About this app')):
+with st.expander(('About this app.')):
     st.markdown((
         '''
     Sprint 6: Software Development Tools - Tripleten
@@ -23,7 +23,7 @@ with st.expander(('About this app')):
 
 
 # Sidebar ----------------------------------------------------
-st.sidebar.header('Select the features your interested in')
+st.sidebar.header("Select the features you're interested in:")
 
 # Sidebar - year selection
 # Get distinct values of 'model_year' and sort them 
@@ -37,12 +37,13 @@ distinct_make = sorted(vehicles_data['make'].unique())
 selected_make = st.sidebar.multiselect('Make/Manufacturer', distinct_make, default=distinct_make)
 
 # Sidebar - condition selection
-# Create list with distunct values for condition
+# Create list with distinct values of condition
 distinct_condition = ['new', 'like new', 'excellent', 'good', 'fair', 'salvage']
 selected_condition = st.sidebar.multiselect('Condition', distinct_condition, default=distinct_condition)
 
 # Sidebar - transmission filter
 automatic = st.sidebar.checkbox('Show only Automatic Transmission')
+
 
 # Filter data based on selections
 filtered_data_prev = vehicles_data[(vehicles_data['model_year']==selected_year) & (vehicles_data['make'].isin(selected_make)) & (vehicles_data['condition'].isin(selected_condition))]
@@ -50,12 +51,23 @@ filtered_data_prev = vehicles_data[(vehicles_data['model_year']==selected_year) 
 # Apply filter for transmision
 if automatic:
     filtered_data = filtered_data_prev[filtered_data_prev['transmission']=='automatic']
+    selected_transmission = ['automatic']
 else:
     filtered_data = filtered_data_prev
+    selected_transmission = ['automatic', 'mechanic']
+
+# Selected Make and Condition
+make_list = ', '.join(selected_make)
+condition_list = ', '.join(selected_condition)
+transmission_list = ', '.join(selected_transmission)
+
 
 # Body -------------------------------------------------------
-hist_section, scatter_section = st.columns(2)
+#Body structure
+hist_section, scatter_section = st.columns(2,gap='medium')
 bars1_section, bars2_section, bars3_section = st.columns(3)
+bottom_container = st.container(border=True)
+
 
 # Body - histogram
 # Create histogram without outliers
@@ -66,24 +78,30 @@ vehicles_data_upd = filtered_data[(filtered_data['price'] < upper_bound) & (filt
 
 with hist_section:
     st.header('Distribution of Prices')
-    st.markdown(f'Distribution by make for the **year {selected_year}**.')
 
-    st.write(px.histogram(vehicles_data_upd, x='price', nbins=50))
+    hist = px.histogram(vehicles_data_upd, x='price', nbins=50, width=450, labels={'price': 'Price ($)'})
+    hist.update_yaxes(title=None)
+
+    st.write(hist)
 
 
 # Body - scatterplot
 with scatter_section:
     st.header('Days Listed vs Price')
-    st.markdown(f'complete')
 
-    st.write(px.scatter(vehicles_data_upd, x='price', y='days_listed',  hover_name='make', hover_data='condition', color='condition'))
+    scatter = px.scatter(vehicles_data_upd, x='price', y='days_listed',  hover_name='make', hover_data='condition', color='condition', width=500, labels={'price': 'Price ($)', 'days_listed': 'Days Listed'})
+    scatter.update_layout(legend=dict(orientation="h", yanchor="top", y=1.1, xanchor="left", x=0.01))
+
+
+    st.write(scatter)
 
 # Body - fuel bars
 # Create barchart fuel
 by_fuel = filtered_data.groupby('fuel').size().reset_index(name='count')
-by_fuel = by_fuel.sort_values(by='count').reset_index(drop=True)
+by_fuel = by_fuel.sort_values(by='count', ascending=False).reset_index(drop=True)
 
-bar_fuel = px.bar(by_fuel, x='fuel', y='count', width=300)
+bar_fuel = px.bar(by_fuel, x='fuel', y='count', width=300, labels={'fuel': 'Fuel'})
+bar_fuel.update_yaxes(title=None)
 
 # Display barchart fuel
 with bars1_section:
@@ -94,8 +112,10 @@ with bars1_section:
 # Body - type bars
 # Create barchart type
 by_type = filtered_data.groupby('type').size().reset_index(name='count')
-by_type = by_type.sort_values(by='count').reset_index(drop=True)
-bar_type = px.bar(by_type, x='type', y='count', width=280)
+by_type = by_type.sort_values(by='count', ascending=False).reset_index(drop=True)
+
+bar_type = px.bar(by_type, x='type', y='count', width=280, labels={'type': 'Type'})
+bar_type.update_yaxes(title=None)
 
 # Display barchart type
 with bars2_section:
@@ -106,11 +126,29 @@ with bars2_section:
 # Body - color bars
 # Create barchart color
 by_color = filtered_data.groupby('paint_color').size().reset_index(name='count')
-by_color = by_color.sort_values(by='count')
-bar_color = px.bar(by_color, x='paint_color', y='count', width=280)
+by_color = by_color.sort_values(by='count', ascending=False)
+
+bar_color = px.bar(by_color, x='paint_color', y='count', width=280, labels={'paint_color': 'Color'})
+bar_color.update_yaxes(title=None)
 
 # Display barchart color
 with bars3_section:
     st.header('Color')
     st.write(bar_color)
-    
+
+
+# Bottom Containes - Selected parameters
+with bottom_container:
+    st.markdown((
+            f'''
+        ##### **Showing data for:**
+        
+        **Year:** {selected_year}
+        
+        **Make/Manufacturer:** {make_list}
+        
+        **Condition:** {condition_list}
+
+        **Transmission:** {transmission_list}
+        '''
+        ))
